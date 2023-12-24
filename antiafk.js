f8eebae6254374621e9032e3f8455eff0f6e1e95
koon-da-pony <koonthepony@gmail.com>
2023-12-24 18:49:16 +0200

    update antiafk.js

--- antiafk.js
+++ antiafk.js
@@ -6,6 +6,7 @@
     console.log("[PT ANTIAFK] STARTING ANTI AFK SUCCESSFULLY!");
 	  setInterval(function () {
 		  canvas.click();
+      console.log("[PT ANTIAFK] CLICK!")
     }, 300000);
   }
 })();
